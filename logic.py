cd266207ce96f820914f17f42fd799615e751bd5
Muhammad Hamiz Ahmed <muhammad-hamiz.ahmed@regenold.com>
2021-07-11 15:08:33 +0200

    Added multithreading and strategy change

--- logic.py
+++ logic.py
@@ -1,15 +1,24 @@
 
 import requests
 import config
-
+import websocket, json
 INITIAL_CAPITAL = 1000
-
+import time
 
 class BuySellCoin:
     def __init__(self):
         self.capital = INITIAL_CAPITAL
         self.coin_balance = 0
         self.transaction_fee = 0.30
+        self.roi = {
+        "0": 0.002,
+        "90": 0.001,
+        "120": 0
+    }
+        self.buy_price = 0
+        self.buy_time = 0
+        self.stoploss = -0.03
+        self.sell_routine_running = False
 
     def get_btc_price(self, API_KEY):
         url = 'https://rest.coinapi.io/v1/exchangerate/BTC/USDT'
@@ -17,7 +26,7 @@ class BuySellCoin:
         response = requests.get(url, headers=headers)
 
         return response.json()["rate"]
-
+        # return 33500
     def buy_coin(self, coin_name, current_price=None):
         if coin_name=="BTC":
             if current_price:
@@ -32,8 +41,56 @@ class BuySellCoin:
                 print("\n\nBought " + coin_name + " at price: ", current_coin_price)
                 print("Total coin balance is: ", self.coin_balance)
                 print("Total capital is: ", self.capital)
+
+                self.buy_price = current_coin_price
+                self.buy_time = time.time()
             else:
                 print("Not enough capital to buy")
+
+    def check_for_sell(self, current_coin_price, ws):
+        self.sell_routine_running = True
+        # minutes_since_bought = (time.time() - self.buy_time) / 60
+        minutes_since_bought = 130
+        if minutes_since_bought > float(list(self.roi.keys())[0]) and minutes_since_bought < float(list(self.roi.keys())[1]):
+            target_percent = self.roi[list(self.roi.keys())[0]]
+
+        elif minutes_since_bought > float(list(self.roi.keys())[1]) and minutes_since_bought < float(list(self.roi.keys())[2]):
+            target_percent = self.roi[list(self.roi.keys())[1]]
+
+        else:
+            target_percent = self.roi[list(self.roi.keys())[2]]
+
+        target_percent = float(target_percent)
+        target_selling_price = (1+target_percent) * self.buy_price
+
+        if target_selling_price == self.buy_price:
+            # adding transaction fee to seeling price
+            target_selling_price = self.buy_price + self.transaction_fee
+
+        stoploss_price = (1+self.stoploss) * self.buy_price
+
+        print("\n\n\nBuying Price: ", self.buy_price)
+        print("Current Price: ", current_coin_price)
+        print("Target: ", target_selling_price)
+        print("Stoploss: ", stoploss_price)
+
+
+        # current_coin_price = self.get_btc_price(config.BTC_PRICE_API_KEY)
+        if current_coin_price > target_selling_price or current_coin_price<stoploss_price:
+            # sell the coin
+            self.capital = self.coin_balance * current_coin_price
+            self.capital = self.capital - self.transaction_fee
+            self.coin_balance = 0
+            print("\nSold " + "BTC" + " at price: ", current_coin_price)
+            print("Total coin balance is: ", self.coin_balance)
+            print("Total capital is: ", self.capital)
+
+            self.buy_price = 0
+            self.buy_time = 0
+            self.sell_routine_running = False
+            print("Closing socket..")
+            ws.close()
+
 
     def sell_coin(self, coin_name):
         if coin_name=="BTC":
@@ -64,8 +121,9 @@ class BuySellCoin:
 
         if signal == "long" and time_unit=="30_minute":
             self.buy_coin(coin, current_coin_price)
-        elif signal=="short" and time_unit=="30_minute":
-            self.sell_coin(coin)
+
+        # elif signal=="short" and time_unit=="30_minute":
+        #     self.sell_coin(coin)
 
     def get_current_balance(self):
         btc_price = self.get_btc_price(config.BTC_PRICE_API_KEY)
@@ -74,3 +132,20 @@ class BuySellCoin:
         return {"portfolio_value": portfolio_value,
                 "current_btc_price": btc_price,
                 "current_capital": self.capital}
+
+    def socket_conn_for_sell(self):
+        def on_message(ws, message):
+            json_msg = json.loads(message)
+            current_price = float(json_msg["k"]["l"])
+            # print("Current_price: ", current_price)
+            self.check_for_sell(current_coin_price=current_price, ws=ws)
+
+        def on_close(ws):
+            print("Connection closed")
+
+        socket = "wss://stream.binance.com:9443/ws/btcusdt@kline_1m"
+        ws = websocket.WebSocketApp(socket,
+                                    on_message=on_message,
+                                    on_close=on_close)
+        self.sell_routine_running = True
+        ws.run_forever()
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, after_this_request
 from logic import BuySellCoin
 from flask import jsonify
+from concurrent.futures import ThreadPoolExecutor
+
+# DOCS https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
+executor = ThreadPoolExecutor(1)
+
 app = Flask(__name__)
 
 
@@ -11,6 +16,8 @@ def respond():
     # print(request.json)
     # print(request.json["test"])
     btc.read_signal(response_json=request)
+    executor.submit(threaded_task)
+
     return Response(status=200)
 
 
@@ -18,6 +25,31 @@ def respond():
 def get_balance():
     balance_dict = btc.get_current_balance()
     return jsonify(balance_dict)
+
+
+def threaded_task():
+    if not btc.sell_routine_running:
+        btc.socket_conn_for_sell()
+
+# @app.route('/socket', methods=['GET'])
+# def socket_conn():
+#     def on_message(ws, message):
+#         print(message)
+#         return Response(status=200)
+#
+#     def on_close(ws):
+#         print("Connection closed")
+#
+#     socket = "wss://stream.binance.com:9443/ws/btcusdt@kline_1m"
+#     ws = websocket.WebSocketApp(socket,
+#                                 on_message=on_message,
+#                                 on_close=on_close)
+#     ws.run_forever()
+
+
+
+
+
 
 
 if __name__=="__main__":
